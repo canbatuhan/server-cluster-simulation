944cda4e852be67f2a55b0f3de22f8f9e681a8a3
ituitis-canb18 <canbatuhan.official@gmail.com>
2022-08-11 21:24:09 +0300

    method implemeentations for Node class

--- servercluster/src/include/ServerCluster.h
+++ servercluster/src/include/ServerCluster.h
@@ -2,6 +2,9 @@
 #include <fstream>
 
 #define DEFAULT_SIZE 5;
+#define DEFAULT_LOG_PATH "_common_log.txt"
+
+using namespace std;
 
 namespace servercluster {
 
@@ -10,27 +13,35 @@ namespace servercluster {
 
     typedef struct Request {
         int id;
-        std::string message;
-        void init(int, std::string);
+        string message;
+        void init(int, string);
         //void clear();
     } Request;
 
     typedef enum NeighbourType {
-        RIGHT,
-        LEFT
+        NEXT,
+        PREVIOUS
     } NeighbourType;
 
     class Node {
         private:
-            int id; std::string name;
+            int id; string name;
             int lowerLimit, upperLimit; // Process range of the node
-            std::fstream logFile;
+            fstream logFile;
             Node* nextNode; Node* previousNode; // Neighbour nodes
+
             void sendToNeighbour(Request*); // Send request to a neighbour
             void processRequest(Request*); // Basically echo the message
             void logProcess(Request*);
 
         public:
+            // Constructors and Destructor
+            Node();
+            Node(int, string, int, int, string);
+            Node(int, string, int, int, string, Node*, Node*);
+            ~Node();
+
+            // Basic Methods
             void setAsNeighbour(Node*, NeighbourType);
             void receiveRequest(Request*); // Receives a request from server or neighbour
     };
@@ -51,7 +62,7 @@ namespace servercluster {
             
             // Basic Methods
             void addServer(Node*);
-            void removeServer(std::string);
+            void removeServer(string);
             void handleRequest(Request*);
 
             // Simulation
@@ -62,5 +73,82 @@ namespace servercluster {
 
     // Function Implementations
     #pragma region
+
+    void Request::init(int requestID, string requestMessage) {
+        id = requestID;
+        message = requestMessage;
+    }
+
+    Node::Node() {
+        this->id = -1;
+        this->name = "";
+        this->lowerLimit = INT16_MIN;
+        this->upperLimit = INT16_MAX;
+        this->logFile.open(DEFAULT_LOG_PATH, fstream::out);
+        this->nextNode = nullptr;
+        this->previousNode = nullptr;
+    }
+
+    Node::Node(int id, string name, int lowerLimit, int upperLimit, string logFilePath) {
+        this->id = id;
+        this->name = name;
+        this->lowerLimit = lowerLimit;
+        this->upperLimit = upperLimit;
+        this->logFile.open(logFilePath, fstream::out);
+        this->nextNode = nullptr;
+        this->previousNode = nullptr;
+    }
+
+    Node::Node(int id, string name, int lowerLimit, int upperLimit, string logFilePath, Node* nextNode, Node* previousNode) {
+        this->id = id;
+        this->name = name;
+        this->lowerLimit = lowerLimit;
+        this->upperLimit = upperLimit;
+        this->logFile.open(logFilePath, fstream::out);
+        this->nextNode = nextNode;
+        this->previousNode = previousNode;
+    }
+
+    Node::~Node() {
+        this->logFile.close();
+        this->nextNode = nullptr;
+        this->previousNode = nullptr;
+    }
+
+    void Node::setAsNeighbour(Node* node, NeighbourType neighbourType) {
+        if (neighbourType == NEXT) {
+            this->nextNode = node;
+        } else {
+            this->previousNode = node;
+        }
+    }
+
+    void Node::receiveRequest(Request* request) {
+        if (request->id < lowerLimit || request->id >= upperLimit) {
+            this->sendToNeighbour(request);
+        } else {
+            this->processRequest(request);
+            this->processRequest(request);
+        }
+    }
+
+    void Node::sendToNeighbour(Request* request) {
+        if (request->id < lowerLimit) {
+            this->previousNode->receiveRequest(request);
+        } else {
+            this->previousNode->receiveRequest(request);
+        }     
+    }
+
+    void Node::processRequest(Request* request) {
+        cout << "Request Type: " << request->id;
+        cout << "\tRequest Message: " << request->message << endl;
+    }
+
+    void Node::logProcess(Request* request) {
+        this->logFile << "[NODE#" << this->name << "] ";
+        this->logFile << "Request Processed - \"" << request->message << "\"" << endl;
+    }
+
     #pragma endregion
 };
